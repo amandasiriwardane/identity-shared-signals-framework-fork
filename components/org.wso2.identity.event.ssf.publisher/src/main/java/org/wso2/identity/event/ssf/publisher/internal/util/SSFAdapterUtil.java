7c06eab3bd2f8f3bf63fe7350a5f87d286bead7e
Amanda Siriwardane <e20378@eng.pdn.ac.lk>
2026-09-08 09:00:00 +0530

    Add application.authentication.framework dependency, printPublisherDiagnosticLog, and SSFCorrelationLogUtils

--- components/org.wso2.identity.event.ssf.publisher/src/main/java/org/wso2/identity/event/ssf/publisher/internal/util/SSFAdapterUtil.java
+++ components/org.wso2.identity.event.ssf.publisher/src/main/java/org/wso2/identity/event/ssf/publisher/internal/util/SSFAdapterUtil.java
@@ -1,10 +1,13 @@
 package org.wso2.identity.event.ssf.publisher.internal.util;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.wso2.carbon.identity.central.log.mgt.utils.LoggerUtils;
 import org.wso2.carbon.identity.event.publisher.api.model.SecurityEventTokenPayload;
+import org.wso2.carbon.utils.DiagnosticLog;
 import org.wso2.identity.event.ssf.publisher.api.exception.SSFAdapterClientException;
 import org.wso2.identity.event.ssf.publisher.api.exception.SSFAdapterServerException;
 import org.wso2.identity.event.ssf.publisher.internal.constant.ErrorMessage;
+import org.wso2.identity.event.ssf.publisher.internal.constant.SSFAdapterConstants;
 
 /**
  * Utility class for SSFAdapter.
@@ -57,4 +60,36 @@ public class SSFAdapterUtil {
         }
         return new SSFAdapterServerException(error.getMessage(), description, error.getCode(), throwable);
     }
+
+    /**
+     * Print diagnostic log for publisher operations.
+     *
+     * @param eventProfileName Name of the event profile.
+     * @param eventProfileUri  URI of the event profile.
+     * @param events           Events published.
+     * @param endpoint         Endpoint URL.
+     * @param action           Action performed.
+     * @param status           Result status.
+     * @param message          Result message.
+     */
+    public static void printPublisherDiagnosticLog(String eventProfileName, String eventProfileUri, String events,
+                                                    String endpoint, String action, DiagnosticLog.ResultStatus status,
+                                                    String message) {
+
+        if (LoggerUtils.isDiagnosticLogsEnabled()) {
+            DiagnosticLog.DiagnosticLogBuilder diagnosticLogBuilder = new DiagnosticLog.DiagnosticLogBuilder(
+                    SSFAdapterConstants.LogConstants.SSF_ADAPTER, action);
+            diagnosticLogBuilder
+                    .inputParam(SSFAdapterConstants.LogConstants.InputKeys.ENDPOINT, endpoint)
+                    .inputParam(SSFAdapterConstants.LogConstants.InputKeys.EVENT_URI, eventProfileUri)
+                    .inputParam(SSFAdapterConstants.LogConstants.InputKeys.EVENT_PROFILE_NAME, eventProfileName)
+                    .inputParam(SSFAdapterConstants.LogConstants.InputKeys.EVENTS, events)
+                    .resultMessage(message)
+                    .resultStatus(status)
+                    .logDetailLevel(DiagnosticLog.LogDetailLevel.APPLICATION);
+            LoggerUtils.triggerDiagnosticLogEvent(diagnosticLogBuilder);
+        }
+    }
+
 }
+
---
+++ components/org.wso2.identity.event.ssf.publisher/src/main/java/org/wso2/identity/event/ssf/publisher/internal/util/SSFCorrelationLogUtils.java
@@ -0,0 +1,82 @@
+package org.wso2.identity.event.ssf.publisher.internal.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This class contains the utility methods for adding correlation logs for the SSF publisher.
+ */
+public class SSFCorrelationLogUtils {
+
+    private static final Log correlationLog = LogFactory.getLog("correlation");
+    private static final String CORRELATION_LOG_SYSTEM_PROPERTY = "enableCorrelationLogs";
+    private static final String CORRELATION_LOG_REQUEST_END = "HTTP-Out-Response";
+    private static final String CORRELATION_LOG_SEPARATOR = "|";
+    private static Boolean isEnableCorrelationLogs;
+
+    public static void handleResponseCorrelationLog(HttpPost request, long requestStartTime, String... otherParams) {
+
+        SSFCorrelationLogUtils.triggerCorrelationLogForResponse(request, requestStartTime, otherParams);
+    }
+
+    public static void triggerCorrelationLogForResponse(HttpEntityEnclosingRequestBase request, long requestStartTime,
+                                                         String... otherParams) {
+
+        if (isCorrelationLogsEnabled() && correlationLog.isInfoEnabled()) {
+            long currentTime = System.currentTimeMillis();
+            long timeTaken = currentTime - requestStartTime;
+
+            List<String> logPropertiesList = new ArrayList<>();
+            logPropertiesList.add(Long.toString(timeTaken));
+            logPropertiesList.add(CORRELATION_LOG_REQUEST_END);
+            logPropertiesList.add(Long.toString(requestStartTime));
+            logPropertiesList.add(request.getMethod());
+            logPropertiesList.add(request.getURI().getQuery());
+            logPropertiesList.add(request.getURI().getPath());
+            Collections.addAll(logPropertiesList, otherParams);
+
+            correlationLog.info(createFormattedLog(logPropertiesList));
+        }
+    }
+
+    private static boolean isCorrelationLogsEnabled() {
+
+        if (isEnableCorrelationLogs == null) {
+            isEnableCorrelationLogs = Boolean.parseBoolean(System.getProperty(CORRELATION_LOG_SYSTEM_PROPERTY));
+        }
+        return isEnableCorrelationLogs;
+    }
+
+    private static String createFormattedLog(List<String> logPropertiesList) {
+
+        return String.join(CORRELATION_LOG_SEPARATOR, logPropertiesList);
+    }
+
+    /**
+     * HTTP outgoing request status.
+     */
+    public enum RequestStatus {
+
+        COMPLETED("completed"),
+        FAILED("failed"),
+        CANCELLED("cancelled");
+
+        private final String status;
+
+        RequestStatus(String status) {
+
+            this.status = status;
+        }
+
+        public String getStatus() {
+
+            return status;
+        }
+    }
+}

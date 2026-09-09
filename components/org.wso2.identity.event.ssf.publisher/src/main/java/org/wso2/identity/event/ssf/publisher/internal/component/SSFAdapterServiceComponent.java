d977ed43dfe9179b70b0583324d71fa152ad3d78
Amanda Siriwardane <e20378@eng.pdn.ac.lk>
2026-09-09 10:00:00 +0530

    Implement real activate() registration in SSFAdapterServiceComponent

--- components/org.wso2.identity.event.ssf.publisher/src/main/java/org/wso2/identity/event/ssf/publisher/internal/component/SSFAdapterServiceComponent.java
+++ components/org.wso2.identity.event.ssf.publisher/src/main/java/org/wso2/identity/event/ssf/publisher/internal/component/SSFAdapterServiceComponent.java
@@ -9,8 +9,14 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.event.publisher.api.service.EventPublisher;
 import org.wso2.carbon.identity.webhook.management.api.service.WebhookManagementService;
+import org.wso2.carbon.identity.webhook.metadata.api.exception.WebhookMetadataException;
 import org.wso2.carbon.identity.webhook.metadata.api.service.EventAdapterMetadataService;
+import org.wso2.identity.event.ssf.publisher.internal.config.SSFAdapterConfiguration;
+import org.wso2.identity.event.ssf.publisher.internal.service.impl.SSFEventPublisherImpl;
+
+import static org.wso2.identity.event.ssf.publisher.internal.constant.SSFAdapterConstants.SSF_ADAPTER_NAME;
 
 /**
  * SSF Outbound Event Adapter service component.
@@ -21,12 +27,33 @@ import org.wso2.carbon.identity.webhook.metadata.api.service.EventAdapterMetadat
 public class SSFAdapterServiceComponent {
 
     private static final Log log = LogFactory.getLog(SSFAdapterServiceComponent.class);
+    private static final String ERROR_CODE_ADAPTER_NOT_FOUND = "WEBHOOKMETA-66011";
 
     @Activate
     protected void activate(ComponentContext context) {
 
-        log.debug("SSF adapter service component activated.");
+        try {
+            SSFAdapterDataHolder.getInstance().setAdapterConfiguration(new SSFAdapterConfiguration(
+                    SSFAdapterDataHolder.getInstance().getEventAdapterMetadataService()
+                            .getAdapterByName(SSF_ADAPTER_NAME).getProperties()));
+            if (SSFAdapterDataHolder.getInstance().getAdapterConfiguration().isAdapterEnabled()) {
+                SSFEventPublisherImpl eventPublisherService = new SSFEventPublisherImpl();
+                context.getBundleContext().registerService(EventPublisher.class.getName(),
+                        eventPublisherService, null);
+                SSFAdapterDataHolder.getInstance().setClientManager(new ClientManager());
+                log.debug("Successfully activated the SSF adapter service.");
+            }
+        } catch (Throwable e) {
+            if (e instanceof WebhookMetadataException &&
+                    ERROR_CODE_ADAPTER_NOT_FOUND.equals(((WebhookMetadataException) e).getErrorCode())) {
+                log.warn("SSF adapter is not enabled. " +
+                        "Please enable the SSF adapter in the configuration file to use the SSF event publisher.");
+            } else {
+                log.error("Error while activating the SSF adapter service: " + e.getMessage(), e);
+            }
+        }
     }
+
 
     @Deactivate
     protected void deactivate(ComponentContext context) {
